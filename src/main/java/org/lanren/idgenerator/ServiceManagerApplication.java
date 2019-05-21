37eaa5859662368c641c02bebc2b737cd7bedefc
LanYuan <launceviya@gmail.com>
2019-05-21 14:54:27 +0800

    init eureka-server

---
+++ src/main/java/org/lanren/idgenerator/ServiceManagerApplication.java
@@ -0,0 +1,23 @@
+package org.lanren.idgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @version V1.0
+ * @ProjectName:service-manager
+ * @Description:
+ * @Copyright: Copyright (c) 2019
+ * @Company:鲸力智享（北京）科技有限公司
+ * @author: Lan Yuan
+ * @email: yuan.lan@jingli365.com
+ * @date 2019-05-20 14:24
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceManagerApplication.class, args);
+    }
+}
